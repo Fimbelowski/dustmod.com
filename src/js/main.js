2a48a6f346a719388b3a168642626ff3185f233d
Fimbelowski <ChrFimbel@gmail.com>
2018-10-27 14:34:17 -0600

    Began styling and setting up Vue for the reactive OS Choice buttons interactions.

--- src/js/main.js
+++ src/js/main.js
@@ -3,13 +3,33 @@ window.onload = function() {
     el: '#l-download',
     data: {
       choicesAreHidden: true,
+
+      windowsIsFocused: false,
+      macIsFocused: false,
+      linuxIsFocused: false,
+
       windowsSubsectionsAreHidden: true,
       macSubsectionsAreHidden: true,
       linuxSubsectionsAreHidden: true
     },
     methods: {
-      showChoices: function() {
+      showOSChoices: function() {
         this.choicesAreHidden = false;
+      },
+      focusOSChoice: function(OSToFocus) {
+        // Reset all OS Choices to be unfocused
+        this.windowsIsFocused = false;
+        this.macIsFocused = false;
+        this.linuxIsFocused = false;
+
+        // Focus the appropriate OS Choice
+        if(OSToFocus === 'windows') {
+          this.windowsIsFocused = true;
+        } else if (OSToFocus === 'mac') {
+          this.macIsFocused = true;
+        } else {
+          this.linuxIsFocused = true;
+        }
       }
     }
   });
