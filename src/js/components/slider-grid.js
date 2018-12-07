04cf425c6aec758c77d08f6f09bc6e72f64f147f
Fimbelowski <chrfimbel@gmail.com>
2018-12-06 18:14:23 -0700

    Added images for feature sliders. Deleted some old images.

--- src/js/components/slider-grid.js
+++ src/js/components/slider-grid.js
@@ -29,24 +29,175 @@ Vue.component('slider-grid', {
                     caption: 'Quality of Life Changes',
                     images: [
                         {
-                            path: 'dist/images/feature-images/multiplayer-1.jpg',
-                            key: 'multiplayer-1',
+                            path: 'dist/images/feature-images/quality-of-life-1.jpg',
+                            key: 'quality-of-life-1',
                             zIndex: 0
                         },
                         {
-                            path: 'dist/images/feature-images/multiplayer-2.jpg',
-                            key: 'multiplayer-2',
+                            path: 'dist/images/feature-images/quality-of-life-2.jpg',
+                            key: 'quality-of-life-2',
                             zIndex: 0
                         },
                         {
-                            path: 'dist/images/feature-images/multiplayer-3.jpg',
-                            key: 'multiplayer-3',
+                            path: 'dist/images/feature-images/quality-of-life-3.jpg',
+                            key: 'quality-of-life-3',
                             zIndex: 0
                         }
                     ],
                     controlsAreLocked: false
                 },
-                
+                {
+                    index: 2,
+                    caption: 'Community Nexus',
+                    images: [
+                        {
+                            path: 'dist/images/feature-images/community-nexus-1.jpg',
+                            key: 'community-nexus-1',
+                            zIndex: 0
+                        },
+                        {
+                            path: 'dist/images/feature-images/community-nexus-2.jpg',
+                            key: 'community-nexus-2',
+                            zIndex: 0
+                        },
+                        {
+                            path: 'dist/images/feature-images/community-nexus-3.jpg',
+                            key: 'community-nexus-3',
+                            zIndex: 0
+                        },
+                        {
+                            path: 'dist/images/feature-images/community-nexus-4.jpg',
+                            key: 'community-nexus-4',
+                            zIndex: 0
+                        },
+                        {
+                            path: 'dist/images/feature-images/community-nexus-5.jpg',
+                            key: 'community-nexus-5',
+                            zIndex: 0
+                        }
+                    ],
+                    controlsAreLocked: false
+                },
+                {
+                    index: 3,
+                    caption: 'Enhanced Custom Level Editor',
+                    images: [
+                        {
+                            path: 'dist/images/feature-images/editor-1.jpg',
+                            key: 'editor-1',
+                            zIndex: 0
+                        },
+                        {
+                            path: 'dist/images/feature-images/editor-2.jpg',
+                            key: 'editor-2',
+                            zIndex: 0
+                        },
+                        {
+                            path: 'dist/images/feature-images/editor-3.jpg',
+                            key: 'editor-3',
+                            zIndex: 0
+                        }
+                    ],
+                    controlsAreLocked: false
+                },
+                {
+                    index: 4,
+                    caption: 'Extended Leaderboards',
+                    images: [
+                        {
+                            path: 'dist/images/feature-images/leaderboard-1.jpg',
+                            key: 'leaderboard-1',
+                            zIndex: 0
+                        },
+                        {
+                            path: 'dist/images/feature-images/leaderboard-2.jpg',
+                            key: 'leaderboard-2',
+                            zIndex: 0
+                        },
+                        {
+                            path: 'dist/images/feature-images/leaderboard-3.jpg',
+                            key: 'leaderboard-3',
+                            zIndex: 0
+                        }
+                    ],
+                    controlsAreLocked: false
+                },
+                {
+                    index: 5,
+                    caption: 'Custom Level Scripting',
+                    images: [
+                        {
+                            path: 'dist/images/feature-images/scripting-1.jpg',
+                            key: 'scripting-1',
+                            zIndex: 0
+                        },
+                        {
+                            path: 'dist/images/feature-images/scripting-2.jpg',
+                            key: 'scripting-2',
+                            zIndex: 0
+                        },
+                        {
+                            path: 'dist/images/feature-images/scripting-3.jpg',
+                            key: 'scripting-3',
+                            zIndex: 0
+                        }
+                    ],
+                    controlsAreLocked: false
+                },
+                {
+                    index: 6,
+                    caption: 'Daily Featured Custom Level',
+                    images: [
+                        {
+                            path: 'dist/images/feature-images/daily-1.jpg',
+                            key: 'daily-1',
+                            zIndex: 0
+                        },
+                        {
+                            path: 'dist/images/feature-images/daily-2.jpg',
+                            key: 'daily-2',
+                            zIndex: 0
+                        },
+                        {
+                            path: 'dist/images/feature-images/daily-3.jpg',
+                            key: 'daily-3',
+                            zIndex: 0
+                        },
+                        {
+                            path: 'dist/images/feature-images/daily-4.jpg',
+                            key: 'daily-4',
+                            zIndex: 0
+                        },
+                        {
+                            path: 'dist/images/feature-images/daily-5.jpg',
+                            key: 'daily-5',
+                            zIndex: 0
+                        }
+                    ],
+                    controlsAreLocked: false
+                },
+                {
+                    index: 7,
+                    caption: 'TAS Tools',
+                    images: [
+                        {
+                            path: 'dist/images/feature-images/tas-tools-1.jpg',
+                            key: 'tas-tools-1',
+                            zIndex: 0
+                        },
+                        {
+                            path: 'dist/images/feature-images/tas-tools-2.jpg',
+                            key: 'tas-tools-2',
+                            zIndex: 0
+                        },
+                        {
+                            path: 'dist/images/feature-images/tas-tools-3.jpg',
+                            key: 'tas-tools-3',
+                            zIndex: 0
+                        }
+                    ],
+                    controlsAreLocked: false
+                }
             ]
         }
     },
