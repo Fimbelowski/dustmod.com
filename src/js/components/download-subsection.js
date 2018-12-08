795d3dbb1a9948126da7398e322e6d9dcd0d8ffa
Fimbelowski <chrfimbel@gmail.com>
2018-12-08 00:51:41 -0700

    Added conditional rendering to download subsections with notes.

--- src/js/components/download-subsection.js
+++ src/js/components/download-subsection.js
@@ -9,6 +9,7 @@ Vue.component('download-subsection', {
       }
     },
     template: "<div class='dl-subsection' :class='[os + \"-subsection\", styles]'>\
+                <p v-if='subsectionInfo.note'>{{ subsectionInfo.note }}</p>\
                 <p v-for='link in subsectionInfo.links'>\
                   <a :href='link.href'>{{ link.text }}</a>\
                 </p>\
