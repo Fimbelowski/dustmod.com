8c2fb52c0ffb6a52d56f990627372a65067c21f0
Fimbelowski <chrfimbel@gmail.com>
2018-12-12 00:53:57 -0700

    Stubbed out functionality for retrieving Dustmod's current version number.

--- src/js/root.js
+++ src/js/root.js
@@ -3,12 +3,28 @@ window.onload = function() {
     el: '#root',
     data: {
       choicesAreShown: false,
-      focusedOS: ''
+      focusedOS: '',
+      version: ''
     },
     methods: {
       focusOS: function(os) {
         this.focusedOS = os;
+      },
+      getVersion: function() {
+        // Query dustkid.com to retrieve Dustmod's current version number
+        var req = new XMLHttpRequest();
+
+        req.open('GET', 'http://dustkid.com/backend8/dustmod_version.php', true);
+
+        req.onload = function() {
+          console.log(this.responseText);
+        }
+
+        req.send();
       }
+    },
+    created: function() {
+      this.getVersion();
     }
   });
 }
