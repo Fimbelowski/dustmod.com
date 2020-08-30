7f598de60a317d253a25ab0b39780b1f21e62c87
Christopher <ChrFimbel@gmail.com>
2020-08-30 17:29:39 -0600

    Hero Image Slider WIP.

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,34 @@
   <v-app>
     <v-main>
       <AppBar/>
+      <HeroImageSlider
+        :images="heroImages"
+      />
     </v-main>
   </v-app>
 </template>
 
 <script>
 import AppBar from './components/AppBar';
+import HeroImageSlider from './components/HeroImageSlider';
 
 export default {
   name: 'App',
 
   components: {
     AppBar,
+    HeroImageSlider,
+  },
+
+  data() {
+    return {
+      heroImages: [
+        {
+          caption: 'infinidifficult',
+          src: 'infini.png',
+        },
+      ],
+    };
   },
 };
 </script>
---
+++ src/components/HeroImageSlider.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container
+    id="hero-image-slider"
+  >
+    <v-row>
+      <v-col
+        :key="image.src"
+        v-for="image in images"
+      >
+        <v-img
+          src="../assets/hero_images/infini.png"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'HeroImageSlider',
+
+  props: {
+    images: {
+      required: true,
+      type: Array,
+    },
+  },
+};
+</script>
