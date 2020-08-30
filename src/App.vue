c024736be56b7acab3d6585187be771205b14362
Christopher <ChrFimbel@gmail.com>
2020-08-30 04:10:19 -0600

    Add AppBar.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
   <v-app>
     <v-main>
+      <AppBar/>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import AppBar from './components/AppBar';
+
 export default {
   name: 'App',
+
+  components: {
+    AppBar,
+  },
 };
 </script>
+
+<style lang="scss">
+@font-face {
+  font-family: 'Caracteres';
+  src: url('./assets/Caracteres L1.ttf') format('truetype');
+}
+</style>
