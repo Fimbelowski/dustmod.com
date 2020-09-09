cb65025062e8de5c70c96b705f8a3719631fc808
Christopher <ChrFimbel@gmail.com>
2020-09-09 09:31:05 -0600

    Add "about" section.

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -1,12 +1,44 @@
 <template>
-  <section>
-    <v-container
-      class="py-8 text-center"
-      fluid
+  <v-container
+    id="about-section"
+    class="py-8"
+    tag="section"
+  >
+    <v-row
+      justify="center"
     >
-      <div />
-    </v-container>
-  </section>
+      <v-col
+        cols="8"
+      >
+        <h2
+          class="mb-8 text-center text-h2"
+        >
+          What is Dustmod?
+        </h2>
+        <v-card>
+          <v-card-text>
+            <blockquote
+              class="body-1"
+            >
+              {{ aboutText }}
+            </blockquote>
+            <div
+              class="text-body-1 text-right"
+            >
+              -msg555, Creator of Dustmod and
+              <a
+                href="http://dustkid.com/"
+                rel="noopener"
+                target="_blank"
+              >
+                dustkid.com
+              </a>
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -20,3 +52,22 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+#about-section {
+
+  blockquote {
+    border-left: 10px solid #757575;
+    padding: 0.5em 10px;
+
+    &:before {
+      color: #757575;
+      content: open-quote;
+      font-size: 4em;
+      line-height: 0.1em;
+      margin-right: 0.25em;
+      vertical-align: -0.4em;
+    }
+  }
+}
+</style>
