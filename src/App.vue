32c6e4a62e6f90cd2e5c5a78ec3095eb8d5925ee
Christopher <ChrFimbel@gmail.com>
2020-09-09 09:54:34 -0600

    Move AboutSection content into App.Vue. Remove AboutSection component.

--- src/App.vue
+++ src/App.vue
@@ -15,13 +15,51 @@
         :images="config.HERO_IMAGES"
         :max-height="heroImageMaxHeight"
       />
-      <AboutSection />
+      <v-container
+        id="about-section"
+        class="py-8"
+        tag="section"
+      >
+        <v-row
+          justify="center"
+        >
+          <v-col
+            cols="8"
+          >
+            <h2
+              class="mb-8 text-center text-h2"
+            >
+              What is Dustmod?
+            </h2>
+            <v-card>
+              <v-card-text>
+                <blockquote
+                  class="body-1"
+                >
+                  {{ aboutText }}
+                </blockquote>
+                <div
+                  class="text-body-1 text-right"
+                >
+                  -msg555, Creator of Dustmod and
+                  <a
+                    href="http://dustkid.com/"
+                    rel="noopener"
+                    target="_blank"
+                  >
+                    dustkid.com
+                  </a>
+                </div>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import AboutSection from './components/AboutSection.vue';
 import HeroImageSlider from './components/HeroImageSlider.vue';
 import images from './assets/hero_images/index';
 
@@ -29,12 +67,12 @@ export default {
   name: 'App',
 
   components: {
-    AboutSection,
     HeroImageSlider,
   },
 
   data() {
     return {
+      aboutText: ' Dustmod is a hobby project that came as a natural extension to dustkid.com, a website that I operate that collects all sorts of Dustforce related statistics. While integrating dustkid.com curated leaderboards into Dustmod and extending the replay system to give richer metadata was an initial goal, the project has expanded to include quite a variety of extended functionality on top of the base game. It\'s my hope that Dustmod continues to be a way to launch deep into the nearly unlimited content that Dustforce and the surrounding community offers.',
       config: {
         HERO_IMAGES: images,
       },
