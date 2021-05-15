5e9531c9298d2a45566d7455eae41f22975360f0
Christopher <Fimbelowski@users.noreply.github.com>
2021-05-14 21:55:46 -0600

    Added main content background tiles. (#18)

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,53 @@
-<script>
-  import Header from './components/Header.svelte';
-</script>
-
 <main>
-  <Header/>
+  <header>
+    <div
+      class="header__inner-container page-width"
+    >
+      <h1
+        class="header__text"
+      >
+        DUSTMOD
+      </h1>
+    </div>
+  </header>
 </main>
+
+<style>
+  @font-face {
+    font-family: 'Caracteres';
+    src: url('/assets/fonts/Caracteres L1.ttf');
+  }
+
+  header {
+    background-image: url('/assets/images/headerTiles.png');
+    background-position: center;
+    box-shadow: 5px -4px 5px 10px rgba(0, 0, 0, .6);
+    box-sizing: border-box;
+    height: 96px;
+  }
+
+  .header__inner-container {
+    align-items: center;
+    display: flex;
+    height: 100%;
+  }
+
+  .header__text {
+    color: white;
+    font-family: Caracteres, sans-serif;
+    font-size: 3rem;
+    margin: 0;
+  }
+
+  main {
+    background: url('/assets/images/backgroundTiles.png');
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  }
+
+  .page-width {
+    margin: auto;
+    max-width: 1000px;
+  }
+</style>
